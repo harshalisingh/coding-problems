d0a8b6d5072f3887198ba51ac0a75f1515339db6
Harshali Singh <singh.harshali@gmail.com>
2016-11-17 15:25:19 -0500

    new problems

--- src/leetcode/Recursion/Combination.java
+++ src/leetcode/Recursion/Combination.java
@@ -3,58 +3,31 @@ package leetcode.Recursion;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-//This One!! EPI Solution
+/*
+ * Given a set of candidate numbers (C) and a target number (T), 
+ * find all unique combinations in C where the candidate numbers sums to T.
+ * The same repeated number may be chosen from C unlimited number of times.
+ * https://leetcode.com/problems/combination-sum/
+ */
 public class Combination {
-	public static void main(String[] args){
-		List<Integer> input = Arrays.asList(0, 1, 2);
-		combination(input);
+	public List<List<Integer>> combinationSum(int[] nums, int target) {
+		List<List<Integer>> list = new ArrayList<>();
+		Arrays.sort(nums);
+		backtrack(list, new ArrayList<>(), nums, target, 0);
+		return list;
 	}
-	public static void combination(List<Integer> inputSet){
-		Map<Integer, Integer> countMap = new TreeMap<>();
-		for (Integer ch : inputSet) {
-			countMap.compute(ch, (key, val) -> {
-				if (val == null) {
-					return 1;
-				} else {
-					return val + 1;
-				}
-			});
-		}
-		int str[] = new int[countMap.size()];
-		int count[] = new int[countMap.size()];
-		int index = 0;
-		for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-			str[index] = entry.getKey();
-			count[index] = entry.getValue();
-			index++;
-		}
-		List<List<Integer>> powerSet = new ArrayList<>();
-		List<Integer> list = new ArrayList<>();
-		combinationUtil(inputSet, 0, new ArrayList<Integer>(), powerSet);
-		System.out.print(powerSet.size());
-		for(List<Integer> set : powerSet){
-			for(Integer num : set){
-				System.out.print(num + ",");
+
+	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+		if(remain < 0) return;
+		else if(remain == 0) list.add(new ArrayList<>(tempList));
+		else{ 
+			for(int i = start; i < nums.length; i++){
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
+				tempList.remove(tempList.size() - 1);
 			}
-			System.out.println();
 		}
 	}
 
-	private static void combinationUtil(List<Integer> inputSet, int pos, List<Integer> outputSet,
-		List<List<Integer>> powerSet) {
-		// TODO Auto-generated method stub
-		if(pos == inputSet.size()){
-			powerSet.add(new ArrayList<>(outputSet));
-			return;
-		}
-
-		outputSet.add(inputSet.get(pos));
-		combinationUtil(inputSet, pos + 1, outputSet, powerSet);
-		outputSet.remove(outputSet.size()-1);
-		combinationUtil(inputSet, pos + 1, outputSet, powerSet);
-
-	}
 }
