d28c3a171fb4ce12900827d621445111908a763f
Harshali Singh <hsingh@dataxu.com>
2017-05-04 16:54:11 -0400

    new problems

--- src/leetcode/Strings/LengthOfLastWord.java
+++
@@ -1,9 +0,0 @@
-package leetcode.Strings;
-
-public class LengthOfLastWord {
-	public int lengthOfLastWord(String s) {
-		s = s.trim();
-	    int lastIndex = s.lastIndexOf(' ') + 1;
-	    return s.length() - lastIndex;        
-	}
-}
